65bf925c9af3f7ac572aab6f495a179580c46d7d
manic-066g <manic-066g@manic-066>
2024-03-21 12:57:31 +0900

    a

--- Jungol/src/q506/Main.java
+++ Jungol/src/q506/Main.java
@@ -4,14 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		int a = 170;
-		double b = 68.600000;
+		double b = 68.6;
 		
-		String bs = String.format("%.7f", b);
+		String bs = String.format("%.6f", b);
 		
 		System.out.println("My height");
 		System.out.println(a);
 		System.out.println("My weight");
 		System.out.println(bs);
+//		System.out.printf("%.7f", b);
 
 	}
 }
