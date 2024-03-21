80002acccc4b4025e1794c6ac4575389489fc685
manic-066g <manic-066g@manic-066>
2024-03-21 12:45:19 +0900

    정올 퀴즈

---
+++ Jungol/src/q506/Main.java
@@ -0,0 +1,17 @@
+package q506;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 170;
+		double b = 68.600000;
+		
+		String bs = String.format("%.7f", b);
+		
+		System.out.println("My height");
+		System.out.println(a);
+		System.out.println("My weight");
+		System.out.println(bs);
+
+	}
+}
